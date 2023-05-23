97c63517cda17f19dbfcf25e788ef65b9bb0b8ee
SinyorDes <sinyordesyt@gmail.com>
2023-05-23 03:57:41 +0300

    Update vyper.sh

--- vyper.sh
+++ vyper.sh
@@ -25,7 +25,7 @@ if [[ "$choice" == "1" ]]; then
             sudo apt update
             sudo apt install git curl build-essential libssl-dev zlib1g-dev libbz2-dev \
                 libreadline-dev libsqlite3-dev wget llvm libncurses5-dev libncursesw5-dev \
-                xz-utils tk-dev libffi-dev liblzma-dev3
+                xz-utils tk-dev libffi-dev liblzma-dev
             git clone https://github.com/pyenv/pyenv.git ~/.pyenv
             echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
             echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
